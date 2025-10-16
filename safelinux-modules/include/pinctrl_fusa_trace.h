2b401b29502ab273eb96fd429eb8fe92fe8003ae
Sankalp Negi <snegi@qti.qualcomm.com>
2025-10-15 23:53:54 -0700

    drivers: Introduce Qualcomm Functional Safety (FuSa) framework support for TLMM
    TLMM FuSa interrupts can be triggered due to 2D CSR parity errors.
    
    The driver registers the handler for TLMM IRQ - tlmm_csr_aggregated_irq,
    and monitors for any type of 2D CSR parity FUSA interrupts.
    
    There are 2 types of such errors:
    1.) AHB_HRESET
    This stands for Advanced High-performance (AHB) Hardware Reset Error
    This error occurs when there is a reset signal on the AHB interface
    2.) POR_ARES
    This stands for Power-on-reset (POR) Asynchronous Reset Error (ARES)
    This error occurs when there is a power-on reset signal error
    
    On the occurrence of the interrupts, the driver does the following:
    Detects the type of error -> Acknowledges the interrupt -> Logs the errors
    
    TLMM FuSa driver handles the registration and handling of all corresponding H/W IRQs introduced in the TLMM FuSA block.
    IRQ handler for these faults, is tasked to read the interrupt status register and then clear the corresponding interrupt.
    
    Below mechanism are made available to convey this information to the
    userspace,
    	a. TraceFS (via trace event)
    	b. DebugFS (via dedicated file per TLMM instance)
    
    Change-Id: I26a239d1129527002adc2c546d547485d10c2762
    Signed-off-by: Sankalp Negi <snegi@qti.qualcomm.com>
    (cherry picked from commit 6fe41876f292cfaa6dbf93849a4d7a96d71d9901)

---
+++ safelinux-modules/include/pinctrl_fusa_trace.h
@@ -0,0 +1,32 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ */
+
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM tlmm_hw_safety
+
+#if !defined(_TRACE_TLMM_HW_SAFETY_H) || defined(TRACE_HEADER_MULTI_READ)
+#define _TRACE_TLMM_HW_SAFETY_H
+
+#include <linux/tracepoint.h>
+
+TRACE_EVENT(tlmm_hw_safety_event,
+	TP_PROTO(const char *interrupt_type, u32 count),
+	TP_ARGS(interrupt_type, count),
+	TP_STRUCT__entry(
+		__string(interrupt_type, interrupt_type)
+		__field(u32, count)
+	),
+	TP_fast_assign(
+		__assign_str(interrupt_type, interrupt_type);
+		__entry->count = count;
+	),
+	TP_printk("Pinctrl FUSA Error: %s, count: %u", __get_str(interrupt_type), __entry->count)
+);
+
+#endif /* _TRACE_TLMM_HW_SAFETY_H */
+#undef TRACE_INCLUDE_PATH
+#define TRACE_INCLUDE_PATH .
+#define TRACE_INCLUDE_FILE pinctrl_fusa_trace
+#include <trace/define_trace.h>
